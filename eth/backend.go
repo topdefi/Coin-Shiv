9db9ecfa57b9cf2f4893829742ba7b3a49f6fcc5
topdefi <94203269+topdefi@users.noreply.github.com>
2022-02-06 15:37:10 -0800

    Update genesis

--- eth/backend.go
+++ eth/backend.go
@@ -168,7 +168,7 @@ func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 	if bcVersion != nil {
 		dbVer = fmt.Sprintf("%d", *bcVersion)
 	}
-	log.Info("Initialising Ethereum protocol", "network", config.NetworkId, "dbversion", dbVer)
+	log.Info("Initialising Shivay protocol", "network", config.NetworkId, "dbversion", dbVer)
 
 	if !config.SkipBcVersionCheck {
 		if bcVersion != nil && *bcVersion > core.BlockChainVersion {
